6a7b3b37c1ae09f32132abf1dc81e7629f98901c
ZhangQin <dev@zhangqin56.cn>
2024-01-25 14:49:48 +0800

    feat: 添加 ExtraDataUtils

--- zq-common-utils/src/main/java/cn/zhangqin56/common/utils/ExtraDataUtils.java
+++ zq-common-utils/src/main/java/cn/zhangqin56/common/utils/ExtraDataUtils.java
@@ -3,8 +3,10 @@ package cn.zhangqin56.common.utils;
 import cn.hutool.core.util.ObjectUtil;
 import lombok.experimental.UtilityClass;
 
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * 数据抽取工具类
@@ -17,9 +19,19 @@ public class ExtraDataUtils {
         return ObjectUtil.isNull(data) ? Optional.empty() : Optional.of(data);
     }
 
-    public <T> T getData(T data, Function<T, T> ref) {
+    /**
+     * 获取 T 的 R 对应的字段，如果 T 为 null，则返回 null
+     */
+    public <T, R> R getData(T data, Function<T, R> ref) {
         return wrapperData(data)
                 .map(ref)
+                .orElse(null);
+    }
+
+    public <E extends Enum<E>> E getEnumBy(Class<E> enumClass, Predicate<? super E> predicate) {
+        return Arrays.stream(enumClass.getEnumConstants())
+                .filter(predicate)
+                .findFirst()
                 .orElse(null);
     }
 }
